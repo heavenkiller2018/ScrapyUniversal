3558759ca113a48eadbab5121084e2e86b765e9c
John <yangyong_th@qq.com>
2019-02-12 12:01:13 +0800

    1

--- scrapyuniversal/utils.py
+++ scrapyuniversal/utils.py
@@ -6,3 +6,10 @@ def get_config(name):
     path = dirname(realpath(__file__)) + '/configs/' + name + '.json'
     with open(path, 'r', encoding='utf-8') as f:
         return json.loads(f.read())
+
+
+if __name__ == '__main__':
+    print(__file__)
+    # print(__file__)
+    print(realpath(__file__))
+    print(dirname(realpath(__file__)))
